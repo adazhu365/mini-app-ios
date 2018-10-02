7ffc5251d4c9841bfc8a00aa40c79e13aab67565
adazhu365 <gz6xw@virginia.edu>
2018-10-01 21:19:58 -0400

    Added add_func

---
+++ uvabucketlist/uvabucketlist/AddViewController.h
@@ -0,0 +1,17 @@
+//
+//  AddViewController.h
+//  uvabucketlist
+//
+//  Created by apple on 2018/10/1.
+//  Copyright © 2018 user145429. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AddViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ uvabucketlist/uvabucketlist/AddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddViewController.swift
+//  uvabucketlist
+//
+//  Created by apple on 2018/10/1.
+//  Copyright © 2018 user145429. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
